22436c4d140ffb37789a8b9830329119faddc124
ZhangX <zxjerry@163.com>
2023-04-07 14:59:26 +0800

    第一次提交

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Air Blog`;
+export const SITE_TITLE = `ZXHubs Blog`;
 export const SITE_DESCRIPTION = 'Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
+export const SITE_EMAIL = 'zxjerry@163.com'
 export const SITE_NAME = 'astro.yufengbiji.com';
 export const SITE_URL = "https://astro.yufengbiji.com";
